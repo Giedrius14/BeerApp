dcd8069f9514f14aa194d5ff565e563c29da576c
Giedrius <AMD-FX>
2016-11-27 20:20:30 +0200

    seperated IT and Unit Tests

--- beerApp/src/test/java/com/giedrius/IT/BeerAppApplicationIT.java
+++ beerApp/src/test/java/com/giedrius/IT/BeerAppApplicationIT.java
@@ -1,4 +1,4 @@
-package com.giedrius;
+package com.giedrius.IT;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BeerAppApplicationTests {
+public class BeerAppApplicationIT {
 
 	@Test
 	public void contextLoads() {
---
+++ beerApp/src/test/java/com/giedrius/Test/BeerAppApplicationTest.java
@@ -0,0 +1,14 @@
+package com.giedrius.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by User on 2016.11.27.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BeerAppApplicationTest {
+    @Test
+    public void firstTest(){}
+}
